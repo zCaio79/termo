db2975be57dfbf3c1ad99c3e2052f5293d8c279d
zCaio79 <caiomendesfaria@gmail.com>
2024-09-26 15:35:47 -0300

    correção/100%

--- Bagui loko/termo.dart
+++ Bagui loko/termo.dart
@@ -1,62 +1,50 @@
-
 import 'dart:io';
 import 'dart:math';
 
 void main() {
-
-  List<String> palavras = ['caule','tiros'];
+  List<String> palavras = ['caule', 'tiros'];
   String sorteio = palavras[Random().nextInt(palavras.length)];
   List<String> letrasCorretas = sorteio.split('');
   int tentativas = 6;
   bool isGameOn = true;
 
-
-  int i = 0;
-  int x = 0;
-
   print('\t\tTermo');
 
-  while (isGameOn) {
-
+  while (isGameOn && (tentativas > 0)) {
     print('\n$tentativas Tentativas disponíveis.');
     print('Digite uma palavra:');
     String? palavraTeste = stdin.readLineSync();
 
-    List<String> resultado = [];
-    
-
-    if (palavraTeste != null && palavraTeste.length == 5){
-      
+    if (palavraTeste != null && palavraTeste.length == 5) {
       List<String> letrasTeste = palavraTeste.split('');
+      List<String> resultado = ['', '', '', '', ''];
 
-      if(palavraTeste == sorteio){
+      if (palavraTeste == sorteio) {
         print("\nPalavra Correta");
         isGameOn = false;
-      }else{
-
-        letrasTeste.forEach((letra) { 
-        letrasCorretas.forEach((letraCorreta) { 
-          if(letra == letraCorreta){
-            resultado.add("[$letra]");
-          }else if((sorteio.contains(letra)) && (letra != letraCorreta)){
-            resultado.add("($letra)");
-          }else{
-            resultado.add(letra);
+      } else {
+        for (int i = 0; i < 5; i++) {
+          if (letrasTeste[i] == letrasCorretas[i]) {
+            resultado[i] = "[${letrasTeste[i]}]";
+          } else if (sorteio.contains(letrasTeste[i])) {
+            resultado[i] = "(${letrasTeste[i]})";
+          } else {
+            resultado[i] = letrasTeste[i];
           }
-        });
-      });
+        }
 
-      for(i = 0; i < resultado.length; i++){
-        print(resultado[i]);
+        print(resultado.join(' '));
       }
 
-      }
-
-      tentativas --;
-
-    }else{
-      print('\n Palavra Inválida, tente uma com 5 letras !');
+      tentativas--;
+    } else {
+      print('\nPalavra Inválida, tente uma com 5 letras!');
     }
-    
+  }
+
+  if (!isGameOn) {
+    print("\nParabéns, você venceu!");
+  } else {
+    print("\nFim de Jogo. A palavra correta era: $sorteio");
   }
 }
